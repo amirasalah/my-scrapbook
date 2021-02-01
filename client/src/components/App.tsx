f399be08c502be498d9da1e5962ff00fa31ee84c
amirasalah <amera.salah.89@gmail.com>
2021-02-01 17:05:36 +0200

    Add portal

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,26 +3,37 @@ import { useDispatch } from 'react-redux'
 import { getPosts } from '../actions/posts'
 import Form from './Form/Form'
 import { Header } from './Header/header'
+import Portal from './Portal/portal'
 import Posts from './Posts/Posts'
 import Container from './Shared/container/container'
 import HalfContainer from './Shared/halfContainer/halfContainer'
 
 const App: FC<any> = () => {
     const [currentId, setCurrentId] = useState<number>(0)
+    const [popUpVisible, setPopUpVisible] = useState(false)
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getPosts())
     }, [currentId, dispatch])
+    const addNewMemory = () => {
+        setPopUpVisible(true)
+    }
     return (
         <>
             <Header />
             <Container className='flex justify-center'>
-                <HalfContainer>
-                    <Form currentId={currentId} setCurrentId={setCurrentId} />
-                </HalfContainer>
+                {popUpVisible && (
+                    <Portal>
+                        <Form
+                            currentId={currentId}
+                            setCurrentId={setCurrentId}
+                        />
+                    </Portal>
+                )}
                 <HalfContainer>
                     <Posts setCurrentId={setCurrentId} />
                 </HalfContainer>
+                <button onClick={addNewMemory}>Add new Memory</button>
             </Container>
         </>
     )
