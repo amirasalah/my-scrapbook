359b9a96f9c82520c5a12410c3ffd9c91184f87a
amirasalah <amera.salah.89@gmail.com>
2021-02-04 12:15:20 +0200

    Fixing the modal

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -4,6 +4,7 @@ import { createPost, updatePost } from '../../actions/posts'
 import HalfContainer from '../Shared/halfContainer/halfContainer'
 import { postInterface } from '../../interfaces'
 import { PopupContext, CurrentIdContext } from '../../context/context'
+import Container from '../Shared/container/container'
 
 const Form: FC<any> = () => {
     const dispatch = useDispatch()
@@ -39,95 +40,110 @@ const Form: FC<any> = () => {
         setPopUpVisible(false)
     }
     const clearForm = () => {
-        setCurrentId('0')
         setPostData(emptyPost)
+        setCurrentId('0')
     }
     return (
-        <>
-            <button onClick={() => setPopUpVisible(false)}>Close</button>
-
-            <h6>
-                {currentId === '0'
-                    ? 'Creating a Memory'
-                    : `Editing "${post.title}"`}
-            </h6>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='creator'>Creator</label>
-                <input
-                    name='creator'
-                    placeholder='Creator'
-                    value={postData.creator}
-                    className='mt-1 block w-full'
-                    type='text'
-                    onChange={e =>
-                        setPostData({
-                            ...postData,
-                            creator: e.target.value,
-                        })
-                    }
-                />
-                <label htmlFor='title'>Title</label>
-                <input
-                    value={postData.title}
-                    type='text'
-                    placeholder='Title'
-                    name='title'
-                    className='mt-1 block w-full'
-                    onChange={e =>
-                        setPostData({
-                            ...postData,
-                            title: e.target.value,
-                        })
-                    }
-                />
-                <label htmlFor='message'>Message</label>
-                <textarea
-                    placeholder='Message'
-                    name='message'
-                    value={postData.message}
-                    className='mt-1 block w-full'
-                    onChange={e =>
-                        setPostData({
-                            ...postData,
-                            message: e.target.value,
-                        })
-                    }
-                />
-                <label htmlFor='tags'>Tags (coma separated)</label>
-                <input
-                    name='tags'
-                    placeholder='Tags'
-                    value={postData.tags}
-                    className='mt-1 block w-full'
-                    type='text'
-                    onChange={e =>
-                        setPostData({
-                            ...postData,
-                            tags: e.target.value.split(','),
-                        })
-                    }
-                />
-                <div className='flex'>
-                    <HalfContainer className='mr-1'>
-                        <button
-                            type='submit'
-                            className='p-1 mt-1 block w-full bg-blue-200'
-                        >
-                            Submit
+        <section className='fixed top-0 right-0 bottom-0 left-0 bg-gray-900 bg-opacity-80'>
+            <Container>
+                <section className='m-32 p-6 bg-gray-50'>
+                    <section className='flex justify-between'>
+                        <h2 className='text-2xl'>
+                            {currentId === '0'
+                                ? 'Creating a Memory'
+                                : `Editing "${post.title}"`}
+                        </h2>
+                        <button onClick={() => setPopUpVisible(false)}>
+                            Close
                         </button>
-                    </HalfContainer>
-                    <HalfContainer className='ml-1'>
-                        <button
-                            type='submit'
-                            className='p-1 mt-1 block w-full bg-blue-200'
-                            onChange={clearForm}
-                        >
-                            Clear Form
-                        </button>
-                    </HalfContainer>
-                </div>
-            </form>
-        </>
+                    </section>
+                    <form onSubmit={handleSubmit}>
+                        <section className='my-4'>
+                            <label htmlFor='creator'>Creator</label>
+                            <input
+                                name='creator'
+                                placeholder='Creator'
+                                value={postData.creator}
+                                className='mt-1 block w-full'
+                                type='text'
+                                onChange={e =>
+                                    setPostData({
+                                        ...postData,
+                                        creator: e.target.value,
+                                    })
+                                }
+                            />
+                        </section>
+                        <section className='my-4'>
+                            <label htmlFor='title'>Title</label>
+                            <input
+                                value={postData.title}
+                                type='text'
+                                placeholder='Title'
+                                name='title'
+                                className='mt-1 block w-full'
+                                onChange={e =>
+                                    setPostData({
+                                        ...postData,
+                                        title: e.target.value,
+                                    })
+                                }
+                            />
+                        </section>
+                        <section className='my-4'>
+                            <label htmlFor='message'>Message</label>
+                            <textarea
+                                placeholder='Message'
+                                name='message'
+                                value={postData.message}
+                                className='mt-1 block w-full'
+                                onChange={e =>
+                                    setPostData({
+                                        ...postData,
+                                        message: e.target.value,
+                                    })
+                                }
+                            />
+                        </section>
+                        <section className='my-4'>
+                            <label htmlFor='tags'>Tags (coma separated)</label>
+                            <input
+                                name='tags'
+                                placeholder='Tags'
+                                value={postData.tags}
+                                className='mt-1 block w-full'
+                                type='text'
+                                onChange={e =>
+                                    setPostData({
+                                        ...postData,
+                                        tags: e.target.value.split(','),
+                                    })
+                                }
+                            />
+                        </section>
+                        <div className='flex'>
+                            <HalfContainer className='mr-1'>
+                                <button
+                                    type='submit'
+                                    className='p-1 mt-1 block w-full bg-blue-200'
+                                >
+                                    Submit
+                                </button>
+                            </HalfContainer>
+                            <HalfContainer className='ml-1'>
+                                <button
+                                    type='submit'
+                                    className='p-1 mt-1 block w-full bg-blue-200'
+                                    onChange={clearForm}
+                                >
+                                    Clear Form
+                                </button>
+                            </HalfContainer>
+                        </div>
+                    </form>
+                </section>
+            </Container>
+        </section>
     )
 }
 
